ae6705c37509cfa751827eb6a8b206fbb5a23d18
MussOo <mustapha.salmi08000@gmail.com>
2024-03-30 14:43:20 +0100

    init BDD + ORM (prisma)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,22 @@
+import PageTitle from "@/components/PageTitle/page";
+import style from "./style.module.scss";
+import { Github, Mail } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className={style.main}>
+      <PageTitle title="Login" />
+      <div className={style.container_btn}>
+        <Button className={style.btn}>
+          <Github size={24} />
+          Authentification avec GitHub
+        </Button>
+        <Button className={style.btn}>
+          <Mail />
+          Authentification avec Google
+        </Button>
+      </div>
+    </div>
+  );
 }
